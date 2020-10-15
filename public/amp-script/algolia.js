d5124deff19f5d6d54de54cdf714c80b9b706317
oriverk <oriveryk@gmail.com>
2020-10-15 02:16:16 +0000

    modified and successed algolia amp-script( but the styles doesnot work though styled-jsx is converting to <style amp-custom />

--- public/amp-script/algolia.js
+++ public/amp-script/algolia.js
@@ -3,52 +3,70 @@
 // glitch - amp-script-demos: html: https://glitch.com/edit/#!/amp-script-demos?path=public%2Falgolia.html%3A142%3A8
 // glitch - amp-script-demos: js: https://glitch.com/edit/#!/amp-script-demos?path=public%2Falgolia.js%3A9%3A0
 
-// const algolia = {
-//   indexName = 'prod_blogOriverkdev',
-//   appId = 'WOHC62BVSP',
-//   apiKey = 'c858d2926cafc635503658417e963722'
-// }
-
-const algolia = {
-  indexName= 'instant_search',
-  appId = 'latency',
-  apiKey = '6be0576ff61c053d5f9a3225e2a90f76'
-}
+const baseUrl = 'https://oriverk.dev'
+const indexName = 'prod_blogOriverkdev'
+const appId = 'WOHC62BVSP'
+const apiKey = 'c858d2926cafc635503658417e963722'
+const referer = 'https://play.oriverk.dev'
 
 const QUERY_URL =
-  `https://${algolia.appId}-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.34.0)%3B%20Browser%20(lite)%3B%20instantsearch.js%20(3.7.0)%3B%20JS%20Helper%20(2.28.0)&x-algolia-application-id=${algolia.appId}&x-algolia-api-key=${algolia.apiKey}`;
+  `https://${appId}-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.34.0)%3B%20Browser%20(lite)%3B%20instantsearch.js%20(3.7.0)%3B%20JS%20Helper%20(2.28.0)&x-algolia-application-id=${appId}&x-algolia-api-key=${apiKey}`;
 
 const searchInput = document.querySelector('#search-input');
 searchInput.addEventListener('input', onInput);
 
 async function onInput(event) {
+
+
   const previousSearchResults = document.querySelector('#search-results');
+
   const newSearchResults = document.createElement('div');
   newSearchResults.setAttribute('id', 'search-results');
 
   if (searchInput.value.length > 0) {
     const response = await runSearch(searchInput.value);
     const results = response.results[0].hits;
+    // {"results":[{"hits":[{"id":"test","title":"only for test","create":"2020-09-01","update":"","tags":["test"],"content":" toc hoge code hoge code hoge code hoge","objectID":"11204930","_highlightResult":{"title":{"value":"__ais-highlight__only__/ais-highlight__ __ais-highlight__for__/ais-highlight__ __ais-highlight__test__/ais-highlight__","matchLevel":"full","fullyHighlighted":true,"matchedWords":["only","for","test"]},"tags":[{"value":"__ais-highlight__test__/ais-highlight__","matchLevel":"partial","fullyHighlighted":true,"matchedWords":["test"]}],"content":{"value":" toc hoge code hoge code hoge code hoge","matchLevel":"none","matchedWords":[]}}}],"nbHits":1,"page":0,"nbPages":1,"hitsPerPage":16,"facets":{},"exhaustiveFacetsCount":true,"exhaustiveNbHits":true,"query":"only for test","params":"query=only for test&hitsPerPage=16&maxValuesPerFacet=10&page=0&removeWordsIfNoResults=allOptional&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&attributesToSnippet=%5B%22description%3A10%22%5D&snippetEllipsisText=%E2%80%A6&facets=%5B%22brand%22%2C%22free_shipping%22%2C%22price%22%2C%22rating%22%2C%22hierarchicalCategories.lvl0%22%5D&tagFilters=","index":"prod_blogOriverkdev","processingTimeMS":1}]}
 
     results.forEach(result => {
       const item = document.createElement('div');
-      item.classList.add('search-item')
+      item.classList.add('search-item');
 
-      // amp-script needs to use AMP components
-      const img = document.createElement('amp-img');
-      img.setAttribute('src', result.image);
-      img.setAttribute('width', 220);
-      img.setAttribute('height', 151);
-      img.setAttribute('layout', 'intrinsic');
-      item.appendChild(img);
+      const anchor = document.createElement('a');
+      anchor.setAttribute('href', `${baseUrl}/posts/${result.id}`);
+      item.appendChild(anchor);
 
-      const label = document.createElement('div');
-      let text = result.name;
-      if (text.length > 50) {
-        text = text.substring(0, 50) + '...';
-      }
-      label.textContent = text;
-      item.appendChild(label);
+      const timeContainer = document.createElement('div')
+      timeContainer.classList.add('datetime')
+      timeContainer.textContent = 'created at '
+      anchor.appendChild(timeContainer)
+      
+
+      const date = document.createElement('time');
+      date.setAttribute('datetime', result.create);
+      date.textContent = result.create;
+      timeContainer.appendChild(date);
+
+      const tags = document.createElement('div');
+      tags.classList.add('tags');
+      result.tags.forEach(t => {
+        const tag = document.createElement('span');
+        tag.textContent = t;
+        tags.appendChild(tag);
+      })
+      anchor.appendChild(tags);
+
+      const title = document.createElement('div');
+      title.classList.add('title');
+      title.textContent = result.title;
+      anchor.appendChild(title);
+
+      // const img = document.createElement('amp-img');
+      // img.setAttribute('src', result.image);
+      // img.setAttribute('width', 220);
+      // img.setAttribute('height', 151);
+      // img.setAttribute('layout', 'intrinsic');
+      // item.appendChild(img);
 
       newSearchResults.appendChild(item);
     });
@@ -56,23 +74,18 @@ async function onInput(event) {
   document.body.replaceChild(newSearchResults, previousSearchResults);
 }
 
-const params = {
-  hitsPer = '16',
-  maxValuesPerFacet = '10',
-}
-
 async function runSearch(query) {
   const headers = {
     accept: 'application/json',
-    Referer: '',
+    Referer: referer,
     'content-type': 'application/x-www-form-urlencoded'
   };
 
   const data = {
     requests: [
       {
-        indexName: algolia.indexName,
-        params: `query=${query}`
+        indexName: indexName,
+        params: `query=${query}&hitsPerPage=16&maxValuesPerFacet=10&page=0&removeWordsIfNoResults=allOptional&highlightPreTag=__ais-highlight__&highlightPostTag=__%2Fais-highlight__&attributesToSnippet=%5B%22description%3A10%22%5D&snippetEllipsisText=%E2%80%A6&facets=%5B%22brand%22%2C%22free_shipping%22%2C%22price%22%2C%22rating%22%2C%22hierarchicalCategories.lvl0%22%5D&tagFilters=`
       }
     ]
   };
--- src/pages/amp/search-mine.tsx
+++ src/pages/amp/search-mine.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import { AmpLayout } from '../../components/AmpLayout'
 import css from 'styled-jsx/css'
 import blogConfig from '../../../blog.config'
@@ -25,14 +24,38 @@ const style = css`
   }
 `
 
+// const algolia = css`
+//   /* amp-scirt for algolia */
+//   .search-item a {
+//     display: block;
+//     padding: .5rem;
+//     text-decoration: none;
+//     color: #EEE;
+//     border-radius: .5rem;
+//     border: 1px solid #EEE;
+//   }
+
+//   .search-item a:hover , .search-item a:active, .search-item a:visited{
+//     background-color: #333;
+//     border: 1px solid #50CAF9;
+//   }
+//   .tag span{
+//     display: inline-block;
+//     font-size: .9rem;
+//     border-radius: 2rem;
+//     border: 1px solid #50CAF9;
+//     padding: 0.1rem .5rem;
+//     margin: .5rem .3rem;
+//     color: #EEE;
+//   }
+// `
+
 const Component: React.FC = () => {
   const algolia = '/amp-script/algolia.js'
   const algoliaSrc = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' + algolia : blogConfig.baseUrl + algolia;
+  // const algoliaSrc = blogConfig.baseUrl + algolia;
   return (
     <AmpLayout>
-      <Head>
-        <meta name='amp-script-src' content='sha384-3WgF_oK2RKt4DfGWiM3YBrsSM8LwiDrLd0hkWIoTHmpDmP3iLQI5GdLYg7zNFGL9' />
-      </Head>
       <div className='container'>
         <div className='content'>
           <amp-script layout='container' src={algoliaSrc} >
@@ -40,12 +63,13 @@ const Component: React.FC = () => {
               type='search'
               id='search-input'
               placeholder='Type to search ... (e.g. google)'
-            />
+              />
             <div id='search-results'>No results</div>
           </amp-script>
         </div>
       </div>
       <style jsx>{style}</style>
+      {/* <style global jsx>{algolia}</style> */}
     </AmpLayout>
   )
 }
